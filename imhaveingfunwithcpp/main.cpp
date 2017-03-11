65385fec16865c9fc1a328dd7882505480822e26
ElectronicsWizardry <brandonhaitken@gmail.com>
2017-03-10 21:16:28 -0800

    Add files via upload
    Well Files, It compiles and run(i think) but its bad.

---
+++ imhaveingfunwithcpp/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int primetest(int testNum);
+
+int main()
+{
+    bool IsAPrime;
+    int userInput;
+    cin >> userInput;
+    IsAPrime == primetest(userInput);
+    cout << IsAPrime;
+    return 0;
+}
+
+int primetest(int testNum){
+
+   /* int primeValToCheck;
+//    cout << i;
+    bool isPrime = false;
+
+    for(int i= primeValToCheck;i > 1; 1--){
+        if(testnum % i == 0){
+            isPrime == true;
+            }
+
+    return isPrime;
+
+    }*/
+    cout << testNum << endl;
+ }
